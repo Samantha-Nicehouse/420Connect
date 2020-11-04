866e1521d2ff4ad680fd44f9c3b51ff5e2504067
Uldis Alksnis <csmccs1@gmail.com>
2020-11-04 18:20:24 +0100

    Naming Fix

--- 420ConnectWebsite/dk.via.420Connect/Startup.cs
+++ 420ConnectWebsite/dk.via.420Connect/Startup.cs
@@ -28,7 +28,6 @@ namespace FourTwentyWebsite
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddScoped<IVendorService, VendorService>();
         }
 
