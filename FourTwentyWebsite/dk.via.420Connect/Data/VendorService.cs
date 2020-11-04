2b0a224f0ea0ffbf8d825f8928aaf071f550f0bb
Samantha Nettesheim <snetteshe@gmail.com>
2020-11-04 18:06:33 +0100

    Registration Vendor

---
+++ FourTwentyWebsite/dk.via.420Connect/Data/VendorService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FourTwentyWebsite.Model;
+
+namespace FourTwentyWebsite.Data
+{
+    public class VendorService : IVendorService
+    {
+        private string vendorsFile = "vendors.json";
+        private IList<Vendor> vendors;
+
+        public VendorService()
+        {
+            if (!File.Exists(vendorsFile))
+            {
+                Seed();
+                WriteVendorsToFile();
+            }
+            else
+            {
+                var content = File.ReadAllText(vendorsFile);
+                vendors = JsonSerializer.Deserialize<List<Vendor>>(content);
+            }
+        }
+
+        public async Task<IList<Vendor>> GetVendorsAsync()
+      
+        {
+            List<Vendor> tmp2 = new List<Vendor>(vendors);
+            return tmp2;
+        }
+
+        public async Task<Vendor> GetVendorById(int id)
+        {
+            List<Vendor> tmp2 = new List<Vendor>(vendors);
+            foreach (var item in tmp2)
+            {
+                if (item.VendorId == id)
+                {
+                    Vendor vendor = item;
+                    return vendor;
+                }
+            }
+
+            return null;
+        }
+
+        public async Task AddVendorAsync(Vendor vendor)
+        {
+            var max = vendors.Max(vendor => vendor.VendorId);
+            vendor.VendorId = ++max;
+            vendors.Add(vendor);
+            WriteVendorsToFile();
+        }
+        
+  
+
+        private void vendorFile()
+        {
+            var productsAsJson = JsonSerializer.Serialize(vendors);
+            File.WriteAllText(vendorsFile, productsAsJson);
+        }
+        public async Task RemoveVendorAsync(int Id)
+        {
+            var toRemove = vendors.First(t => t.VendorId == Id);
+           vendors.Remove(toRemove);
+            vendorFile();
+        }
+        private void Seed()
+        {
+            Vendor[] ps =
+            {
+                new Vendor
+
+                {
+                   
+                    FirstName= "Yeshua",
+                    LastName= "Magana",
+                    VendorName= "Black",
+                    VendorId= 11111,
+                    Email = "snetteshe@gmail.com",
+                    PhoneNumber = 111111111,
+                    City = "Wilmington",
+                    Country = "USA"
+                    
+                },
+                new Vendor
+                {
+                    FirstName= "Joe",
+                    LastName= "Trump",
+                    VendorName= "BigCitySmoke",
+                    VendorId= 13434311,
+                    Email = "jkklkhihio@gmail.com",
+                    PhoneNumber = 1234535,
+                    City = "Wilmington",
+                    Country = "USA"
+
+                }
+            };
+            vendors = ps.ToList();
+        }
+
+        private void WriteVendorsToFile()
+        {
+            var productsAsJson = JsonSerializer.Serialize(vendors);
+            File.WriteAllText(vendorsFile, productsAsJson);
+        }
+
+       
+    }
+    }
---
+++ FourTwentyWebsite/dk.via.420Connect/Model/Vendor.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Text.RegularExpressions;
+
+namespace FourTwentyWebsite.Model
+{
+    public class Vendor
+    
+    {
+        
+        [JsonPropertyName("vendorId")]
+        public int VendorId { get; set; }
+        
+        [JsonPropertyName("vendorName")] public string VendorName { get; set; }
+
+        [NotNull]
+        [JsonPropertyName("firstName")]
+        public string FirstName { get; set; }
+
+        [NotNull]
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+        
+        [Required]
+        [JsonPropertyName("eMail")]
+        public string Email { get; set; }
+        
+        [Required]
+        [JsonPropertyName("phoneNumber")]
+        public int PhoneNumber { get; set; }
+        
+        [Required]
+        [JsonPropertyName("vendorLicense")]
+        public string vendorLicense { get; set; }
+        
+        [Required]
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+        
+        [Required]
+        [JsonPropertyName("country")]
+        public string Country { get; set; }
+        
+        
+    }
+   
+
+    public class IsValidEmail{
+        public static bool EmailCheck(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                    RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+
+    
+}
